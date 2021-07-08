544b794eb8d63a1777091f4486745641310a41ab
Brunovera180 <soybrunovera@gmail.com>
2021-07-08 08:08:40 -0300

    Fin del Proyecto

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express();
 
 // Router
 import IndexRoutes from './routes/index.routes'
-
+import TaskRoutes from './routes/tasks.routes'
 // Settings
 app.set('port', process.env.PORT || 3000);
 
+// Middlewares
+app.use(json());
+
+
 // Routes
 app.use(IndexRoutes);
-
+app.use('/tasks', TaskRoutes);
 
 export default app;
